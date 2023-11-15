6e0e7f71ff6412c74b8b5d2e77f978cfea99a9e3
Yasitha Sanjula Herath <yasithasanjula2018@gmail.com>
2023-11-16 02:26:59 +0530

    111

--- 3D-Slider/src/App.jsx
+++ 3D-Slider/src/App.jsx
@@ -1,16 +1,81 @@
-import react from 'react'
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import {EffectCoverflow, Pagination ,Navigation} from 'swiper';
+
+
+import img1 from './assets/images/img1.jpg';
+import img2 from './assets/images/img2.jpg';
+import img3 from './assets/images/img3.jpg';
+import img4 from './assets/images/img4.jpg';
+import img5 from './assets/images/img5.jpg';
+import img6 from './assets/images/img6.jpg';
+import img7 from './assets/images/img7.jpg';
 
 function App() {
- 
   return (
-    <>
-      <div>
-        <h1>Yasitha</h1>
+    <div className="container">
+      <h1 className="heading">Flower Gallery</h1>
+      <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        loop={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 2.5,
+        }}
+        pagination={{ el: '.swiper-pagination', clickable: true }}
+        navigation={{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+          clickable: true,
+        }}
+        modules={[EffectCoverflow, Pagination, Navigation]}
+        className="swiper_container"
+      >
+        <SwiperSlide>
+          <img src={slide_image_1} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_2} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_3} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_4} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_5} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_6} alt="slide_image" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide_image_7} alt="slide_image" />
+        </SwiperSlide>
+
+        <div className="slider-controler">
+          <div className="swiper-button-prev slider-arrow">
+            <ion-icon name="arrow-back-outline"></ion-icon>
+          </div>
+          <div className="swiper-button-next slider-arrow">
+            <ion-icon name="arrow-forward-outline"></ion-icon>
+          </div>
+          <div className="swiper-pagination"></div>
         </div>
-       
-    </>
-  )
+      </Swiper>
+    </div>
+  );
 }
 
-export default App
+export default App;
